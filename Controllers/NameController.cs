49b37bb2858f07550f9a80ee7184af37b30d6a21
Samir Agrawal <agrawasu@mail.uc.edu>
2023-07-15 19:47:39 -0400

    removed weather forecast and added name controller

---
+++ Controllers/NameController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace samir_agrawal_versioncontrol.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NameController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetName()
+        {
+            string name = "Samir Agrawal";
+
+            return Ok(new { name });
+        }
+    }
+}
+
+
